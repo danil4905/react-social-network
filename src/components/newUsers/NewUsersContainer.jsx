2e79369e85ad499e55ef8bbe93424348735b0076
Danil <conovalov.danil3441@gmail.com>
2020-03-23 22:26:02 +0400

    Version 51 (REST API)
    Use axios npm

--- src/components/newUsers/NewUsersContainer.jsx
+++ src/components/newUsers/NewUsersContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NewUsers from "./NewUsers";
-import { followAC, unfollowAC,setUsersAC } from '../../redux/users-reducer';
+import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
+
 const mapStateToProps = (state) => {
     return {
         users: state.usersPage.users
@@ -20,4 +21,6 @@ const mapDispatchToProps = (dispatch) => {
         }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(NewUsers)
+debugger;
+const NewUsersContainer = connect(mapStateToProps, mapDispatchToProps)(NewUsers);
+export default NewUsersContainer;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,54 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 let initialState = {
   users: [
-    {
-      id: 1,
-      fullName: "Natalya",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: true
-    },
-    {
-      id: 2,
-      fullName: "Armen",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: false
-    },
-    {
-      id: 3,
-      fullName: "Denis",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: true
-    },
-    {
-      id: 4,
-      fullName: "David",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: false
-    },
-    {
-      id: 5,
-      fullName: "Rudick",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: true
-    },
-    {
-      id: 6,
-      fullName: "Maxim",
-      imageUrl: '#',
-      status: "I am boss",
-      location: { country: "Russia", city: "Izhevsk" },
-      followed: true
-    }
+    
   ]
 };
 
@@ -87,11 +40,11 @@ const usersReducer = (state = initialState, action) => {
       return state;
   }
 };
-export default usersReducer;
-export const followAC = userId => ({ type: FOLLOW, userId });
 
+export const followAC = userId => ({ type: FOLLOW, userId });
 export const unfollowAC = userId => ({
   type: UNFOLLOW,
   userId
 });
-export const setUsersAC = users => ({ typy: SET_USERS, users });
+export const setUsersAC = users => ({ type: SET_USERS, users });
+export default usersReducer;
