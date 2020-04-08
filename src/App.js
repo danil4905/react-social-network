72648df0b2013d8f7c4859b2cef2da0eba138b44
Danil <conovalov.danil3441@gmail.com>
2020-04-08 22:59:21 +0400

    Update App

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import NewUsersContainer from "./components/newUsers/NewUsersContainer";
 import ProfileContainer from "./components/profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
 import Login from "./components/login/Login";
-function App(props) {
-  return (
-    <BrowserRouter>
-      <HeaderContainer />
-      <div className="App-wrapper">
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-          <Route path="/users" render={() => <NewUsersContainer />} />
-          <Route path="/login" render={() => <Login />} />
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <HeaderContainer />
+        <div className="App-wrapper">
+          <Navbar />
+          <div className="app-wrapper-content">
+            <Route path="/dialogs" render={() => <DialogsContainer />} />
+            <Route
+              path="/profile/:userId?"
+              render={() => <ProfileContainer />}
+            />
+            <Route path="/users" render={() => <NewUsersContainer />} />
+            <Route path="/login" render={() => <Login />} />
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
